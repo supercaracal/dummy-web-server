2397ea99397ea7d11a7d741b4d6d79353792ac51
Taishi Kasuga <kasuga@livesense.co.jp>
2023-09-21 21:02:29 +0900

    Add health check

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +13,7 @@ import (
 const (
 	contentTypeKey  = "Content-Type"
 	contentTypeJSON = "application/json;charset=UTF-8"
+	bindAddr        = "0.0.0.0"
 	defaultPort     = 3000
 	rootDir         = "/"
 )
@@ -21,7 +23,25 @@ var (
 )
 
 func main() {
-	port := defaultPort
+	port := getPortNumber(defaultPort)
+
+	if isSubCommand("health") {
+		if err := checkHealth(port, rootDir); err != nil {
+			log.Fatalf("NG: %s", err)
+		}
+
+		log.Printf("OK")
+		os.Exit(0)
+	}
+
+	setHandler(rootDir, responseBody)
+
+	log.Printf("listen: %s:%d", bindAddr, port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", bindAddr, port), nil))
+}
+
+func getPortNumber(defaultNumber int) int {
+	port := defaultNumber
 	if v := os.Getenv("PORT"); v != "" {
 		if n, err := strconv.Atoi(v); err == nil && n > 1023 {
 			port = n
@@ -31,13 +51,43 @@ func main() {
 	flag.IntVar(&port, "port", port, "listen port number")
 	flag.Parse()
 
-	http.HandleFunc(rootDir, func(w http.ResponseWriter, r *http.Request) {
+	return port
+}
+
+func isSubCommand(cmd string) bool {
+	if len(os.Args) == 0 {
+		return false
+	}
+
+	for _, arg := range os.Args {
+		if arg == cmd {
+			return true
+		}
+	}
+
+	return false
+}
+
+func checkHealth(port int, dir string) error {
+	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d%s", port, dir))
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if _, err := io.ReadAll(resp.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setHandler(dir string, body []byte) {
+	http.HandleFunc(dir, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(contentTypeKey, contentTypeJSON)
 		w.WriteHeader(http.StatusOK)
-		w.Write(responseBody)
+		w.Write(body)
 		log.Print(r.URL.Path)
 	})
-
-	log.Printf("listen: 0.0.0.0:%d", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), nil))
 }
